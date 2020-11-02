e05171b1825586d4b5aa9369a3283f1d01a988b5
Exr0n <spotyie@gmail.com>
2020-11-02 15:48:45 -0800

    works on sample data; results vary by starting locations

--- kmeans/main.py
+++ kmeans/main.py
@@ -1,11 +1,13 @@
+# test data from https://www.kaggle.com/shrutimechlearn/customer-data
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
 from random import randint
 from math import sqrt
 
 ITERATIONS = 10
-MEANS = 10
+MEANS = 5
 
 minx=1e9
 miny=1e9
@@ -19,26 +21,23 @@ def dist(p1, p2):
     return sqrt(abs(p1[0]-p2[0])**2 + abs(p1[1]-p2[1]))
 
 if __name__ == '__main__':
-    try:
-        MEANS = int(input("How many clusters? "))
-    except:
-        pass
+    # try:
+    #     MEANS = int(input("How many clusters? "))
+    # except:
+    #     pass
+    #
+    # num_pts = int(input("How many points? "))
+    # data = np.zeros((num_pts, 2), dtype=float)
+    # for i in range(num_pts):
+    #     data[i][0], data[i][1] = map(lambda n: float(n), input("Point? ").strip().split(' '))
+    #     # TODO not dry
+    #     minx = min(minx, data[i][0])
+    #     maxx = max(maxx, data[i][0])
+    #     miny = min(miny, data[i][1])
+    #     maxy = max(maxy, data[i][1])
 
-    num_pts = int(input("How many points? "))
-    data = np.zeros((num_pts, 2), dtype=float)
-    for i in range(num_pts):
-        data[i][0], data[i][1] = map(lambda n: float(n), input("Point? ").strip().split(' '))
-        # TODO not dry
-        minx = min(minx, data[i][0])
-        maxx = max(maxx, data[i][0])
-        miny = min(miny, data[i][1])
-        maxy = max(maxy, data[i][1])
-
-    # centers = np.zeros((MEANS, 2))
-    # for i in range(MEANS):
-    #     centers[i] = [randint(minx, maxx), randint(miny, maxy)]
-
-    # centers = np.array(np.random.choice(data, MEANS, replace=False))    # why does it have to be 1d bruh
+    raw_data = pd.read_csv('data/mall_customers.csv')
+    data = raw_data.iloc[:, [3, 4]].values  # annual income, spending score
 
     centers = np.random.permutation(data)[:MEANS]   # TODO sketchy + inefficient random.sample
 
@@ -56,8 +55,7 @@ if __name__ == '__main__':
             # remember in cluster data
             count[cluster] += 1
             sums [cluster] += p
-        centers = sums / np.tile(count, (2, 1)).T
-        plt.scatter(*centers.T, color=[1-it/ITERATIONS]*3)
-        print(centers)
+        centers = sums / np.tile(count, (2, 1)).T               # update the centers
+        plt.scatter(*centers.T, color=[1-it/ITERATIONS]*3)      # plot iteration on chart
     plt.show()
 
